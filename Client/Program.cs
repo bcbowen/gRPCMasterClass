89d28bb283b6d3ac74c4bf3b8a064311f5eef21b
Ben Bowen <bcbowen@gmail.com>
2021-11-18 22:23:45 -0500

    Adding demo files

---
+++ Client/Program.cs
@@ -0,0 +1,45 @@
+﻿using Dummy;
+using Greet;
+//using Calculate;
+using Grpc.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    class Program
+    {
+        const string target = "127.0.0.1:50051";
+        static async Task Main(string[] args)
+        {
+            Channel channel = new Channel(target, ChannelCredentials.Insecure);
+            channel.ConnectAsync().ContinueWith((task) => 
+            {
+                if (task.Status == TaskStatus.RanToCompletion)
+                    Console.WriteLine("This client connected successfully, man");
+
+                Console.WriteLine(task.Status);
+            });
+
+            //var client = new DummyService.DummyServiceClient(channel);
+            var client = new GreetingService.GreetingServiceClient(channel);
+
+            var greeting = new Greeting()
+            {
+                FirstName = "Ben", 
+                LastName = "Bowen"
+            };
+
+            var request = new GreetingRequest() { Greeting = greeting };
+            var response = client.Greet(request);
+
+            Console.WriteLine(response.Result); 
+
+            channel.ShutdownAsync().Wait();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,42 @@
+﻿using Greet;
+using Grpc.Core;
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    class Program
+    {
+        const int Port = 50051;
+        static void Main(string[] args)
+        {
+            Grpc.Core.Server server = null;
+            try
+            {
+                server = new Grpc.Core.Server()
+                {
+                    Services = { GreetingService.BindService(new GreetingServiceImpl()) },
+                    Ports = { new ServerPort("localhost", Port, ServerCredentials.Insecure) }
+                };
+                server.Start();
+                Console.WriteLine($"The server is listening on port: {Port}");
+                Console.ReadKey();
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("The server failed to start: " + e.Message);
+                throw;
+            }
+            finally 
+            {
+                if (server != null)
+                    server.ShutdownAsync().Wait();
+            }
+        }
+    }
+}
