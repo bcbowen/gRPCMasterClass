94d1ccac64415c4ddadb3a8977335a9739e691c7
Ben Bowen <bcbowen@gmail.com>
2021-11-25 17:15:35 -0500

    Set up blog service demo

---
+++ Server/BlogServiceImpl.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using static Blog.BlogService;
+using Grpc.Core;
+
+namespace Server
+{
+    public class BlogServiceImpl : BlogServiceBase
+    {
+
+    }
+}
